930a8f18cc4e3fd0d439824c17037ad242f12346
Max Burke <maxburke@gmail.com>
2013-01-05 11:23:53 -0800

    Fixing some warnings, fixed an unintended use of = instead of == for CMPN_EQ

--- base.h
+++ base.h
@@ -1,6 +1,18 @@
 #ifndef BASE_H
 #define BASE_H
 
+/*
+ * Pragma wrappers.
+ */
+#ifdef _MSC_VER
+    #define DISABLE_WARNING(x) __pragma(warning(disable:x))
+#else
+    #define DISABLE_WARNING(x)
+#endif
+
+/*
+ * Debug break macro.
+ */
 #ifdef _MSC_VER
 #   define BREAK() __debugbreak()
 #else
@@ -8,6 +20,9 @@
 #   define BREAK() raise(SIGTRAP)
 #endif
 
+/*
+ * Wrappers around the inline keyword.
+ */
 #if defined(_MSC_VER)
     /*
      * CL doesn't provide inline but it provides __inline
@@ -24,6 +39,9 @@
     #define inline __inline__
 #endif
 
+/*
+ * Formatting strings for systems that don't have inttypes.h
+ */
 #ifdef _MSC_VER
     #define PRId64 "I64d"
 #else
