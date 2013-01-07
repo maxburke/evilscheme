3c213136ef862ab32c747f937bf6496e021e56e2
Max Burke <maxburke@gmail.com>
2013-01-06 16:19:51 -0800

    Create an executable on Windows.

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
 
-for %%a in (*.c) do cl /c /W4 /Wall /WX /wd4668 /wd4820 /wd4711 /nologo /Zi %%a
+for %%a in (*.c) do cl /c /W4 /Wall /WX /wd4668 /wd4820 /wd4711 /nologo /Zi /Od %%a
 
+link /nologo /DEBUG /out:r4rs.exe /PDB:r4rs.pdb *.obj
