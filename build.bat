8670a85b747961f06703e553a7050167a4559fe8
Max Burke <maxburke@gmail.com>
2013-01-31 21:23:54 -0800

    Build in parallel.

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 
-for %%a in (*.c) do cl /c /W4 /Wall /WX /wd4668 /wd4820 /wd4711 /nologo /Zi /Od %%a
+::for %%a in (*.c) do cl /c /W4 /Wall /WX /wd4668 /wd4820 /wd4711 /nologo /Zi /Od %%a
+cl /c /W4 /Wall /WX /wd4668 /wd4820 /wd4711 /nologo /Zi /Od /MP *.c
 
 link /nologo /DEBUG /out:r4rs.exe /PDB:r4rs.pdb *.obj
