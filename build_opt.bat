4a592224667fdbdc7f331be54fe548d4bb828a1c
Max Burke <maxburke@gmail.com>
2013-02-07 22:53:54 -0800

    Slight revision to the Windows build batchfiles. If you're watching aghast at the use of batch files, it was easier than nmake and everything builds in less than a second. So whatevs.

--- build_opt.bat
+++ build_opt.bat
@@ -1,4 +1,6 @@
 @echo off
 
-for %%a in (*.c) do cl /Ox /c /W4 /Wall /WX /wd4668 /wd4820 /wd4711 /nologo /Zi %%a
+cl /Ox /c /W4 /Wall /WX /wd4668 /wd4820 /wd4711 /nologo /Zi /MP *.c
+
+link /nologo /DEBUG /out:r4rs.exe /PDB:r4rs.pdb *.obj
 
